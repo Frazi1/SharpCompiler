b6e3949df12fd46db2107d4d0438e9b3faee1347
Fraz <diman896@gmail.com>
2017-12-14 23:50:44 +0300

    Generated var index instructions.

---
+++ InstuctionsCodeGenerator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstuctionsCodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            First();
+        }
+
+        static void First()
+        {
+            var list = File.ReadAllLines(@"..\..\VariablesCommands.txt");
+            List<string> result = new List<string>();
+            foreach (string varCommand in list)
+            {
+                result.AddRange(GenerateVarCommandClass(varCommand));
+            }
+            File.WriteAllLines("varCommands.txt", result);
+        }
+
+        private static IEnumerable<string> GenerateVarCommandClass(string varCommand)
+        {
+            var splitted = varCommand.Split(' ');
+            string name = splitted[0];
+            return GeneratePublicInstructionClass(name);
+
+        }
+
+        private static IEnumerable<string> GeneratePublicInstructionClass(string name)
+        {
+            List<string> result = new List<string>();
+            result.Add($"public sealed class {name}Instruction : IJasminInstruction");
+            result.Add("{");
+            result.Add($"public string Name"+ "{get;} = " + $"\"{name}\";");
+            result.Add("public int VarIndex {get;}");
+            result.Add($"public {name}Instruction(int varIndex)");
+            result.Add("{");
+            result.Add("VarIndex = varIndex;");
+            result.Add("}");
+            result.Add("}");
+            return result;
+        }
+    }
+}
---
+++ JasminSharp/Modules/Instructions/JasminIndexedInstruction.cs
@@ -0,0 +1,122 @@
+﻿using MathLang.CodeGeneration.JasminJava;
+
+public sealed class retInstruction : IJasminInstruction
+{
+    public string Name { get; } = "ret";
+    public int VarIndex { get; }
+
+    public retInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class aloadInstruction : IJasminInstruction
+{
+    public string Name { get; } = "aload";
+    public int VarIndex { get; }
+
+    public aloadInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class astoreInstruction : IJasminInstruction
+{
+    public string Name { get; } = "astore";
+    public int VarIndex { get; }
+
+    public astoreInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class dloadInstruction : IJasminInstruction
+{
+    public string Name { get; } = "dload";
+    public int VarIndex { get; }
+
+    public dloadInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class dstoreInstruction : IJasminInstruction
+{
+    public string Name { get; } = "dstore";
+    public int VarIndex { get; }
+
+    public dstoreInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class floadInstruction : IJasminInstruction
+{
+    public string Name { get; } = "fload";
+    public int VarIndex { get; }
+
+    public floadInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class fstoreInstruction : IJasminInstruction
+{
+    public string Name { get; } = "fstore";
+    public int VarIndex { get; }
+
+    public fstoreInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class iloadInstruction : IJasminInstruction
+{
+    public string Name { get; } = "iload";
+    public int VarIndex { get; }
+
+    public iloadInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class istoreInstruction : IJasminInstruction
+{
+    public string Name { get; } = "istore";
+    public int VarIndex { get; }
+
+    public istoreInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class lloadInstruction : IJasminInstruction
+{
+    public string Name { get; } = "lload";
+    public int VarIndex { get; }
+
+    public lloadInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
+
+public sealed class lstoreInstruction : IJasminInstruction
+{
+    public string Name { get; } = "lstore";
+    public int VarIndex { get; }
+
+    public lstoreInstruction(int varIndex)
+    {
+        VarIndex = varIndex;
+    }
+}
