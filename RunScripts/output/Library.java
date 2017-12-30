acad8315e4f82bb451a5bbb2e572eaef5e29fe2b
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-12-30 22:05:25 +0300

    Added Library source file.

---
+++ RunScripts/output/Library.java
@@ -0,0 +1,17 @@
+class Library {
+	public static void printString(String s){
+		System.out.println(s);
+	}
+	
+	public static void printInt(int i){
+		System.out.println(i);
+	}
+	
+	public static void printChar(char c){
+		System.out.println(c);
+	}
+	
+	public static void printBool(boolean b){
+		System.out.println(b);
+	}
+}
